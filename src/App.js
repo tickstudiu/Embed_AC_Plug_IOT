1ade2d9fdeb9c1bc0425325e4411ecd33ad79a8a
PalatipReal <sungsilver3@gmail.com>
2019-10-15 22:59:33 +0700

    Add DB

--- src/App.js
+++ src/App.js
@@ -9,7 +9,14 @@ class App extends Component {
   constructor(props){
   super(props);
   var config = {
-
+    apiKey: "AIzaSyBVbhVhCXfWZ4OtaXSxB3UC1gW9JGq3pbA",
+    authDomain: "embedapp-9157e.firebaseapp.com",
+    databaseURL: "https://embedapp-9157e.firebaseio.com",
+    projectId: "embedapp-9157e",
+    storageBucket: "embedapp-9157e.appspot.com",
+    messagingSenderId: "271166942683",
+    appId: "1:271166942683:web:66dc2b700bc620c89b0bfe",
+    measurementId: "G-S9Y65TP0RC"
   };
   firebase.initializeApp(config);
 }
