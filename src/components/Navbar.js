f091bb3d46068e97366fe2573bf32d4c26b92118
Wanchalerm S <tickstudiu@gmail.com>
2019-10-18 11:10:11 +0700

    update navbar using contaier

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,30 +6,31 @@ import {
   NavbarBrand,
   Nav,
   NavItem,
-  NavLink
+  NavLink,
+  Container
 } from 'reactstrap';
 import { Link } from "react-router-dom";
 
 export default class Navba extends Component {
   render() {
     return (
-      <div>
-        <Navbar color="warning" light expand="md">
+      <Navbar color="warning" light expand="md">
+        <Container>
           <Link to="/">
-              <NavbarBrand style={{ fontWeight: "bold", fontSize: 30, color: "White" }}>
-                Home
+            <NavbarBrand style={{ fontWeight: "bold", fontSize: 30, color: "White" }}>
+              Home
               </NavbarBrand>
           </Link>
           <NavbarToggler />
           <Collapse navbar>
             <Nav className="ml-auto" navbar>
               <NavItem>
-              <Link to="control"><NavLink style={{ fontWeight: "bold", fontSize: 20, color: "White" }} href="/control" >Control</NavLink></Link>
+                <Link to="control"><NavLink style={{ fontWeight: "bold", fontSize: 20, color: "White" }} href="/control" >Control</NavLink></Link>
               </NavItem>
             </Nav>
           </Collapse>
-        </Navbar>
-      </div>
+        </Container>
+      </Navbar>
     )
   }
 }
